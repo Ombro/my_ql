91fac7d6721237bfe483b279aa2e2455479e3d60
huanghongbiao.hhb <huanghongbiao.hhb@alibaba-inc.com>
2023-10-12 17:59:49 +0800

    #ql-3 毒鸡汤 新增随机延迟

--- ql_push_soul.py
+++ ql_push_soul.py
@@ -4,11 +4,18 @@ import requests
 import json
 import datetime
 import math
+import random
+from time import sleep
+
 from utils import utils_notify
 from config import config_soul_lists
 
 soul_lists = config_soul_lists.soul_lists
 start_time = 1696953600 # 2023-10-11 00:00:00
+
+def wait_random_sec():
+    rand = random.randint(1, 3600*4 - 1)
+    sleep(rand)
 
 def get_index():
     now_ts = int(datetime.datetime.now().timestamp())
@@ -30,7 +37,7 @@ def format_msg_body():
     content_list = format_content()
     length = len(content_list)
     msg_body = {}
-    msg_body['template_id'] = "DQzCGZrNVDHKWg7oyUTZcSYkDdLnp5m4N_Mr4jZ87Ds"
+    msg_body['template_id'] = "P2493j8-C3AurRXAPxzoCqbphDUVVXS613oKi-0vfKQ"
     msg_body['data'] = {}
     for i in range(length):
         value = ""
@@ -46,4 +53,5 @@ def format_msg_body():
 if __name__ == '__main__':
     msg_body = format_msg_body()
     notify = utils_notify.Notify()
+    wait_random_sec()
     notify.notify(msg_body)
